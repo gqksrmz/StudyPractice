234267d5fe9eececc601ddb69ec648cf1cab9bbe
1825346742@qq.com <1825346742@qq.com>
2019-08-30 17:09:26 +0800

    文件上传

---
+++ StudyPractice/Asp.NetDemo1/Page/UploadFile/TestUploadFile.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="TestUploadFile.aspx.cs" Inherits="Asp.NetDemo1.Page.UploadFile.TestUploadFile" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            请选择你要上传的文件：
+            <br />
+            <asp:FileUpload ID="fileUpload" runat="server" />
+            <br />
+            <asp:Button ID="btnUpload" runat="server" Text="开始上传" OnClick="btnUpload_Click" />
+            <br />
+            <asp:Literal ID="litMsg" runat="server"></asp:Literal>
+        </div>
+    </form>
+</body>
+</html>
---
+++ StudyPractice/Asp.NetDemo1/Page/UploadFile/TestUploadFile.aspx.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Asp.NetDemo1.Page.UploadFile
+{
+    public partial class TestUploadFile : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnUpload_Click(object sender, EventArgs e)
+        {
+            //1.判断文件是否存在
+            if (!this.fileUpload.HasFile) return;
+            //2.判断文件大小，判断是否符合设置要求(文件大小要变成MB)
+            double fileLength = this.fileUpload.FileContent.Length / (1024.0 * 1024.0);
+            //3.获取配置文件中对上传文件大小的限制
+            double limitedLength = Convert.ToDouble(System.Configuration.ConfigurationManager.AppSettings["PhysicsObjectLength"]);
+            limitedLength = limitedLength / 1024.0;
+            //4.判断实际文件大小是否符合要求
+            if (fileLength > limitedLength)
+            {
+                //this.litMsg.Text = "上传文件大小不能超过" + limitedLength + "MB";
+                this.litMsg.Text = "<script type='text/javascript'>alert('上传文件最大不能超过"+limitedLength+"M')</script>";
+                return;
+            }
+            //4.获取文件名，判断文件扩展名是否符合要求
+            string fileName = this.fileUpload.FileName;
+            if (fileName.Substring(fileName.LastIndexOf(".")).ToLower() == ".exe")
+            {
+                this.litMsg.Text= "<script type='text/javascript'>alert('上传文件不能是exe文件')</script>";
+                return;
+            }
+            //5.修改文件 abc.doc 改成年月日 20190830_文件名.扩展名
+            fileName = DateTime.Now.ToString("yyyyMMddhhssms") + "_" + fileName;
+            //6.获取服务器文件夹的路径
+            string path = Server.MapPath("~/上传的文件");
+            //7.上传文件
+            try
+            {
+                this.fileUpload.SaveAs(path + "/" + fileName);
+                this.litMsg.Text= "<script type='text/javascript'>alert('文件上传成功！')</script>";
+            }
+            catch(Exception ex)
+            {
+                this.litMsg.Text = "<script type='text/javascript'>alert('文件上传失败！"+ex.Message+"')</script>";
+
+            }
+
+        }
+    }
+}
