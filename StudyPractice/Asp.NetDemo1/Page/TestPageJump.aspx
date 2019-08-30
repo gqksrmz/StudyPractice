0649fc868b33dc9a85b56b0530c3a1722ed646bb
1825346742@qq.com <1825346742@qq.com>
2019-08-30 08:34:53 +0800

    页面跳转和输出浏览器信息

---
+++ StudyPractice/Asp.NetDemo1/Page/TestPageJump.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="TestPageJump.aspx.cs" Inherits="Asp.NetDemo1.Page.TestPageJump" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <a href="TestPageJump1.aspx?name=root&age=20">跳转到第二个页面</a>
+        </div>
+    </form>
+</body>
+</html>
---
+++ StudyPractice/Asp.NetDemo1/Page/TestPageJump1.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Asp.NetDemo1.Page
+{
+    public partial class TestPageJump1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //string name = Page.Request.QueryString["name"];
+            //string age = Page.Request.QueryString["age"];
+            string name = Page.Request.Params["name"];
+            string age = Page.Request.Params["age"];
+            Page.Response.Write("姓名是" + name + ",年龄是" + age);
+            Response.Write("<br></br>");
+            //获取浏览器版本信息
+            string iexplorev = Request.ServerVariables["HTTP_USER_AGENT"];
+            //获取浏览器语言
+            string iexplorel = Request.ServerVariables["HTTP_ACCEPT_LANGUAGE"];
+            //输出信息
+            Response.Write("浏览器机器版本信息：" + iexplorev + "<br></br>");
+            Response.Write("浏览器使用的语言" + iexplorel);
+             
+        }
+    }
+}
