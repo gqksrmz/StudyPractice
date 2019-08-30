da4eacb8d7eed2c59237360629eda4f65d9fdf1e
1825346742@qq.com <1825346742@qq.com>
2019-08-30 13:47:40 +0800

    使用Cookie存储用户名

---
+++ StudyPractice/Asp.NetDemo1/Page/TestCookie/TestCookie.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="TestCookie.aspx.cs" Inherits="Asp.NetDemo1.Page.TestSession.TestSession" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            请输入用户名:<asp:TextBox ID="textBox1" runat="server"></asp:TextBox>
+            <asp:Button ID="btnSave" runat="server" Text="将用户名保存到Cookie" OnClick="btnSave_Click" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ StudyPractice/Asp.NetDemo1/Page/TestCookie/TestCookie.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Asp.NetDemo1.Page.TestSession
+{
+    public partial class TestSession : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                if (Request.Cookies["userName"] != null)
+                {
+                    Response.Write("用户名=" + Request.Cookies["userName"].Value);
+                }
+                if (Request.Cookies["userPhone"] != null)
+                {
+                    Response.Write("用户电话=" + Request.Cookies["userPhone"].Value);
+                }
+            }
+        }
+
+        protected void btnSave_Click(object sender, EventArgs e)
+        {
+            Response.Cookies["userName"].Expires = DateTime.Now.AddDays(1.0);
+            Response.Cookies["userName"].Value = this.textBox1.Text.Trim().ToString();
+
+            HttpCookie httpCookie = new HttpCookie("userPhone", "1234567890");
+            httpCookie.Expires = DateTime.Now.AddDays(1.0);
+            Response.Cookies.Add(httpCookie);
+        }
+    }
+}
