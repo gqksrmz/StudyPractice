d3e26cd89d26225944aab15a290e8249dd2dbd46
1825346742@qq.com <1825346742@qq.com>
2019-08-31 08:28:50 +0800

    重写了一遍

--- StudyPractice/Asp.NetDemo1/Page/UploadFile/TestUploadFile.aspx.cs
+++ StudyPractice/Asp.NetDemo1/Page/UploadFile/TestUploadFile.aspx.cs
@@ -16,43 +16,46 @@ namespace Asp.NetDemo1.Page.UploadFile
 
         protected void btnUpload_Click(object sender, EventArgs e)
         {
+
             //1.判断文件是否存在
             if (!this.fileUpload.HasFile) return;
-            //2.判断文件大小，判断是否符合设置要求(文件大小要变成MB)
+            //2.获取文件大小，判断是否符合设置要求(变成MB)
             double fileLength = this.fileUpload.FileContent.Length / (1024.0 * 1024.0);
-            //3.获取配置文件中对上传文件大小的限制
-            double limitedLength = Convert.ToDouble(System.Configuration.ConfigurationManager.AppSettings["PhysicsObjectLength"]);
-            limitedLength = limitedLength / 1024.0;
+            //3.获取配置文件中上传文件大小的限制
+            double limitedLength = Convert.ToDouble(System.Configuration.ConfigurationManager
+                .AppSettings["PhysicsObjectLength"]);
+            limitedLength = limitedLength / 1024.0;//转换成MB单位
             //4.判断实际文件大小是否符合要求
             if (fileLength > limitedLength)
             {
-                //this.litMsg.Text = "上传文件大小不能超过" + limitedLength + "MB";
-                this.litMsg.Text = "<script type='text/javascript'>alert('上传文件最大不能超过"+limitedLength+"M')</script>";
+                //this.litMsg.Text = "上传文件大小不能超过" + limitedLength;
+                this.litMsg.Text = "<script type='text/javascript'>alert('上传文件最大不能超过"+limitedLength+"')</script>";
                 return;
             }
             //5.获取文件名，判断文件扩展名是否符合要求
             string fileName = this.fileUpload.FileName;
+            //6.判断文件名是否是exe文件
             if (fileName.Substring(fileName.LastIndexOf(".")).ToLower() == ".exe")
             {
-                this.litMsg.Text= "<script type='text/javascript'>alert('上传文件不能是exe文件')</script>";
+                this.litMsg.Text = "<script type='text/javascript'>alert('上传文件大小不能是.exe文件')</script>";
                 return;
             }
-            //6.修改文件 abc.doc 改成年月日 20190830_文件名.扩展名
+            //7.修改文件名 abc.doc 改成年月日时分秒毫秒_文件名.扩展名
             fileName = DateTime.Now.ToString("yyyyMMddhhssms") + "_" + fileName;
-            //7.获取服务器文件夹的路径
+            //8.获取服务器文件夹路径
             string path = Server.MapPath("~/上传的文件");
-            //8.上传文件
+            //9.上传文件
             try
             {
                 this.fileUpload.SaveAs(path + "/" + fileName);
-                this.litMsg.Text= "<script type='text/javascript'>alert('文件上传成功！')</script>";
+                this.litMsg.Text = "<script type='text/javascript'>alert('文件上传成功！')</script>";
+
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 this.litMsg.Text = "<script type='text/javascript'>alert('文件上传失败！"+ex.Message+"')</script>";
 
             }
-
         }
     }
 }
