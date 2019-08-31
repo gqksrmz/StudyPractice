bbcb6a39bd83c37a4b0ea97a9fbd1f4c6337cf80
1825346742@qq.com <1825346742@qq.com>
2019-08-31 12:53:53 +0800

    学生管理系统

---
+++ StudentManagerPro/DAL/AdminService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+namespace DAL
+{
+    public class AdminService
+    {
+    }
+}
